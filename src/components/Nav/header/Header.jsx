1ae7adabf58fec1dfe6b1c0afb3492dd78eca911
Sergio Delgado <giancarlo080700@gmail.com>
2023-11-06 19:51:51 -0500

    advances

--- src/components/Nav/header/Header.jsx
+++ src/components/Nav/header/Header.jsx
@@ -8,20 +8,20 @@ function Header() {
     <>
       <div className='header__div'>
         <figure className='header__figure'>
-          <img className='figure__img' src="../../../../public/avatar.svg" alt="" />
+          <img className='figure__img' src="/avatar.svg" alt="" />
         </figure>
         <h3 className='header__subtitle'>Hola, soy Sergio</h3>
         <h2 className='header__title'>Diseñador y </h2>
         <h2 className='header__title'>desarrollador web</h2>
         <div className='header__networks'>
           <a class="div_a" href="https://www.linkedin.com/" target="_blank">
-            <img src="../../../../Íconos/linkedin.svg" alt="icono linkedin" />
+            <img src="/Íconos/linkedin.svg" alt="icono linkedin" />
           </a>
           <a class="div__a" href="https://www.instagram.com/" target="_blank">
-            <img src="../../../../public/Íconos/instagram.svg"alt="icono instagram" />
+            <img src="/Íconos/instagram.svg"alt="icono instagram" />
           </a>
           <a class="div__a" href="https://twitter.com/" target="_blank">
-            <img src="../../../../public/Íconos/twitter.svg" alt="icono twitter" />
+            <img src="/Íconos/twitter.svg" alt="icono twitter" />
             </a>
         </div>
 
